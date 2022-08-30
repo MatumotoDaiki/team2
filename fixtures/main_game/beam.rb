d2aec33c837a0096c7a883355bdc28bcc6edbfc9
Miyawaki-raku <1717miyawaki@gmail.com>
2022-08-30 10:46:24 +0900

    #1 player

--- fixtures/main_game/beam.rb
+++ fixtures/main_game/beam.rb
@@ -1,1 +1,16 @@
+module Fixture::MainGame
+  class Beam < Sprite
+    def initialize(x, y)
+      self.x = x
+      self.y = y
+      @speed = -3
+      self.image = Image.load("images/player.png")
+      self.image.set_color_key(C_WHITE)
+    end
 
+    def update
+      self.y += @speed
+      self.draw
+    end
+  end
+end
