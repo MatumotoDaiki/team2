fe8ad2ece1a2c242ab14fa8755b5c57d7a4fb8a0
kiji-haru <kijiharu3112@gmil.com>
2022-09-01 14:25:33 +0900

    opening and how to play

--- scene/how_to_play.rb
+++ scene/how_to_play.rb
@@ -3,10 +3,16 @@ module Scene
 
     def update
       super
-      Window.draw_font(100, 100, "move:W, A, S, D", Font.new(32))
-      Window.draw_font(Window.width, 100, "attack:SPACE key", Font.new(32))
+      move_background_right_and_draw
+
+      Window.draw_font(Window.width / 2 - 200, 100, "move : W, A, S, D", Font.new(32))
+      Window.draw_font(Window.width / 2 - 200, Window.height / 2, "attack : SPACE key", Font.new(32))
+      Window.draw_font(Window.width / 2 - 180, Window.height - 100, "Please click or push space key", Font.new(25))
     end
 
+    def next_scene
+      Scene::Opening.new
+    end
     def finish?
       return true if Input.mouse_push?(M_LBUTTON) || Input.key_push?(K_SPACE)
       false
--- scene/opening.rb
+++ scene/opening.rb
@@ -3,20 +3,24 @@ module Scene
     include Fixture::Select # for use Mouse class
     def initialize
       super
-      @next_scene
+      @next_scene = Scene::Select.new
       @string = "Space or Enter"
       question_image = Image.load("images/question.png")
       @mouse = Mouse.new
+      @question_click = false
       @Question = Sprite.new(Window.width - question_image.width, question_image.height, question_image)
     end
 
     def update
       super
-      @Question.draw
-      if @mouse === @Question && Input.mouse_push?(M_LBUTTON)
-
-      end
       move_background_right_and_draw
+
+      @Question.draw
+      @mouse.update
+      if @mouse === @Question && Input.mouse_push?(M_LBUTTON)
+        @next_scene = Scene::How.new
+        @question_click = true
+      end
       Window.draw_font(
         Window.width/2 - @string.length/2 * Setting::TITLE_FONT_SIZE/2,
         Window.height/2 - Setting::TITLE_FONT_SIZE/2,
@@ -24,7 +28,7 @@ module Scene
     end
 
     def next_scene
-      Scene::Select.new
+      @next_scene
     end
 
     def finish?
@@ -33,6 +37,7 @@ module Scene
       decide_key.each do |key|
         return true if Input.key_push?(key)
       end
+      return true if @question_click
       false
     end
   end
